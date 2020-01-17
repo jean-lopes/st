434b9d3494560c66a701e1a390aeda3185cca6c8
Jean Lopes <jean.lopes@hotmail.com.br>
2020-01-17 13:38:29 -0300

    PC Master race

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@
  * 	  0 = no border
  * 	100 = border width is same as cell width
  */
-static char *font = "Source Code Pro:Regular:size=16:antialias=true:autohint=true";
+static char *font = "Hack-Regular:Regular:size=16:antialias=true:autohint=true";
 int borderperc = 20;
 
 /*
@@ -45,7 +45,7 @@ static unsigned int tripleclicktimeout = 600;
 int allowaltscreen = 1;
 
 /* frames per second st should at maximum draw to the screen */
-static unsigned int xfps = 120;
+static unsigned int xfps = 144;
 static unsigned int actionfps = 60;
 
 /*
